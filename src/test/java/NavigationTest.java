9dfb93d28997d56044bea9ff76b2e8f31769e332
Phavuk <43843255+Phavuk@users.noreply.github.com>
2020-03-18 17:14:25 +0100

    Navigation test

---
+++ src/test/java/NavigationTest.java
@@ -0,0 +1,38 @@
+import org.junit.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.*;
+
+public class NavigationTest {
+    private WebDriver driver;
+    private final String BASE_URL = "http://localhost/";
+
+    @Before
+    public void setUp(){
+        System.setProperty("webdriver.chrome.driver","src/test/resources/drivers/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+    @Test
+    public void Test(){
+        String expectedCLass = "active";
+        driver.get(BASE_URL+"vybersi.php");
+        System.out.println(driver.findElement(By.xpath("//li[a/@href='vybersi.php']")).getAttribute("class"));
+        Assert.assertTrue(driver.findElement(By.xpath("//li[a/@href='vybersi.php']"))
+                .getAttribute("class")
+                .contains(expectedCLass));
+
+        driver.get(BASE_URL+"tabulka.php");
+        Assert.assertTrue(driver.findElement(By.xpath("//li[a/@href='tabulka.php']"))
+                .getAttribute("class")
+                .contains(expectedCLass));
+
+        driver.get(BASE_URL+"zjavenie.php");
+        Assert.assertTrue(driver.findElement(By.xpath("//li[a/@href='zjavenie.php']"))
+                .getAttribute("class")
+                .contains(expectedCLass));
+    }
+    @After
+    public void tearDown(){
+
+    }
+}
